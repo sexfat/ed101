cdbbabdf8f12cf7d3b225497642c33d0b31f6377
sexfat <bryant6831@gmail.com>
2020-04-24 10:48:16 +0800

    add

--- js/animation.js
+++ js/animation.js
@@ -1,1 +1,13 @@
 console.log('start');
+
+
+
+TweenMax.to('.box_basic', 1, {
+    x: '200px',
+    y: '200px',
+    repeat: -1,
+    // ease: Power0.easeNone,
+    ease: Back.easeOut,
+    repeatDelay:1,
+    // yoyo:true
+});
